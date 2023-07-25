152f811581a1cdc54433d414c2ef7da71d373b86
lautaroh2394 <lautarohaase_23@hotmail.com>
2023-07-24 21:58:28 -0300

    add logic to show saved podcasts

--- src/View.js
+++ src/View.js
@@ -3,20 +3,20 @@ import { isAuthorized } from './spotify-utils.js';
 const pageModel = {
   start: (_) => {
     pageModel.hideAllBlocks();
+    pageModel.configureSaved();
     pageModel.showLoading();
 
     if (!isAuthorized()) {
       pageModel.askLogin();
     } else {
       pageModel.showSearch();
+      pageModel.hideLoading();
     }
   },
   askLogin: (_) => {
-    pageModel.showSearch();
     pageModel.showAuth();
   },
   showSearch: (_) => {
-    pageModel.hideAllBlocks();
     pageModel.show('#search');
     pageModel.show('#results');
   },
@@ -27,7 +27,7 @@ const pageModel = {
     pageModel.hide('#loading');
   },
   hideAllBlocks: (_) => {
-    [...document.querySelectorAll('body > #container > div')].forEach((block) => {
+    [...document.querySelectorAll('body > .container > div')].forEach((block) => {
       pageModel.hide(block);
     });
   },
@@ -51,8 +51,21 @@ const pageModel = {
   show: (sel) => {
     document.querySelector(sel).style.display = '';
   },
-  toggleOffline: (type) => {
-    if (type === 'offline') pageModel.show('#nointernet');
+  configureSaved: () => {
+    let saved = localStorage.getItem('saved-podcasts');
+    if (!saved) return;
+    pageModel.show('#SavedPodcasts');
+    saved = JSON.parse(saved);
+    saved.items.forEach((podcast) => {
+      // todo
+    });
+  },
+  showSavedPodcasts: () => {
+    pageModel.show('#SavedPodcasts');
+    pageModel.show('#SavedPodcastsList');
+  },
+  toggleOffline: (status) => {
+    if (status === 'offline') pageModel.show('#nointernet');
     else pageModel.hide('#nointernet');
   },
 };
--- src/dom-utils.js
+++ src/dom-utils.js
@@ -205,7 +205,7 @@ const emptyList = (_) => {
 };
 
 const setEvents = (_) => {
-  autorizar.addEventListener('click', (ev) => {
+  Authorize.addEventListener('click', (ev) => {
     window.location = 'https://accounts.spotify.com/authorize?client_id=76fb0a13403d4147b10d78950a9fe7d2&redirect_uri=https%3A%2F%2Flautaroh2394.github.io&response_type=token';
   });
 
@@ -243,11 +243,10 @@ const setEvents = (_) => {
     pageModel.toggleOffline(ev.type);
   });
 
-  /*
-    SavedPodcasts.addEventListener("click", ev => {
-        //todo
-    })
-    */
+  SavedPodcasts.addEventListener('click', (ev) => {
+    pageModel.hideAllBlocks();
+    pageModel.showSavedPodcasts();
+  });
 };
 
 const setSW = (_) => {
